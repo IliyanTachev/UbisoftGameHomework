cd3393e145feed76b54df753ca49942bb865ee9c
IliyanTachev <iliqn.tachev@gmail.com>
2018-05-21 14:00:34 +0300

    second homework added

---
+++ Second Homework/Assets/Scripts/EnemyProjectile.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour {
+    public float speed = 6f;
+    public int health = 100;
+    // Use this for initialization
+    void Start()
+    {
+        var Rb = GetComponent<Rigidbody>();
+        Rb.velocity = transform.forward * speed;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "PlayerShip")
+        {
+            health -= 10;
+            if (health == 0)
+            {
+                Destroy(collision.gameObject);
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
